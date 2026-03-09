c2d70912a146b6e5ad290b1be024e23d649ce47d
Salem <loucks@umich.edu>
2026-03-09 13:44:00 -0400

    navigating corners

--- animated.js
+++ animated.js
@@ -1,18 +1,36 @@
 const video = document.getElementById('intro-video');
 const overlay = document.getElementById('intro-overlay');
 const content = document.getElementById('main-content');
+const links = document.querySelectorAll('.interactive-logo');
 
 // When the MP4 finishes playing...
 video.onended = () => {
-    // 1. Start the fade out of the video
     overlay.style.opacity = '0';
     
-    // 2. Start the fade in of the logos
-    content.classList.remove('hidden');
-    content.style.opacity = '1';
-
-    // 3. Completely remove the video overlay after fade finishes
     setTimeout(() => {
         overlay.style.display = 'none';
+        document.body.style.overflow = 'auto';
     }, 800); // This must match the CSS transition time
 };
+
+
+links.forEach(link => {
+    link.addEventListener('click', function(e) {
+        // 1. Stop the browser from leaving the page immediately
+        e.preventDefault();
+        const destination = this.href;
+
+        content.classList.add('navigating');
+        content.classList.remove('no-navigation');
+
+        // need to have this zoom out overrule the hover thingy
+        
+        // 3. Mark this specific link as 'clicked'
+        this.classList.add('clicked');
+
+        // 4. Wait for the animation (0.7s) then change the page
+        //setTimeout(() => {
+            //window.location.href = destination;
+        //}, 700); 
+    });
+});
