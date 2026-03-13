8b887c4e97e47313e1043d19e7302b93e29d9ca6
Salem <loucks@umich.edu>
2026-03-13 10:32:33 -0400

    added links + radio project

--- animated.js
+++ animated.js
@@ -27,23 +27,24 @@ links.forEach(link => {
  
 const projectData = {
     code: [
-        { title: "SatBird Multi-Model Ensemble", desc: "Improved predictions by 20% on SatBird state-of-the-art benchmark for crowdsourced bird encounter rates in Michigan and designed multi-model ensemble: CNNs + MLP with environmental and satellite datacubes, with a preprocessing pipeline to remove clouds", img: "code/satbird.png", link: "#" },
-        { title: "3D/4D Human Mesh Reconstruction", desc: "Built 3D human reconstruction with SMPL, camera initialization, differentiable mesh generation, multi-stage optimization. Integrated 2D joint detectors with the transformer-based 4DHumans models to benchmark performance across images and video", img: "code/4Dhuman.png", link: "#" },
-        { title: "Global Nominee Exoplanet Detector at NASA SpaceApps", desc: "Designed a Conditional Variational Autoencoder (cVAE) and synthetic data generator to model stellar variability and transits. Implemented NUFFT-based signal processing and adaptive interpolation for incomplete time-series data using Python and C++ extensions, with signal embeddings in a React dashboard", img: "code/spaceapps.png", link: "#" },
-        { title: "SongChaser, 3rd Best Use of Intel AI award at MHacks", desc: "Built an ensemble of K-Nearest Neighbors (KNN) models to create paths of similar songs based on Spotify features. Integrated model and pathfinding with a Flask backend to connect to React frontend and host a demo version", img: "code/songchaser.png", link: "#" }
+        { title: "SatBird Multi-Model Ensemble", desc: "Improved predictions by 20% on SatBird state-of-the-art benchmark for crowdsourced bird encounter rates in Michigan and designed multi-model ensemble: CNNs + MLP with environmental and satellite datacubes, with a preprocessing pipeline to remove clouds", img: "code/satbird.png", link: "https://github.com/evanteal15/SatBird_Group9" },
+        { title: "3D/4D Human Mesh Reconstruction", desc: "Expanded on 4D humans transformer-based skinning project: Built 3D human reconstruction with SMPL, camera initialization, differentiable mesh generation, multi-stage optimization. Integrated 2D joint detectors with the 4DHumans models to benchmark performance across images and video", img: "code/4Dhuman.png", link: "https://shubham-goel.github.io/4dhumans/" },
+        { title: "Global Nominee Exoplanet Detector at NASA SpaceApps", desc: "Designed a Conditional Variational Autoencoder (cVAE) and synthetic data generator to model stellar variability and transits. Implemented NUFFT-based signal processing and adaptive interpolation for incomplete time-series data using Python and C++ extensions, with signal embeddings in a React dashboard", img: "code/spaceapps.png", link: "https://www.spaceappschallenge.org/2025/find-a-team/exoghost/?tab=project" },
+        { title: "SongChaser, 3rd Best Use of Intel AI award at MHacks", desc: "Built an ensemble of K-Nearest Neighbors (KNN) models to create paths of similar songs based on Spotify features. Integrated model and pathfinding with a Flask backend to connect to React frontend and host a demo version", img: "code/songchaser.png", link: "https://devpost.com/software/songchaser" }
     ],
     art: [
+        { title: "Alien Radio (2026)", desc: "My most recent project! Blender image + textures from scratch, music collected carefully for different channels, voice-acted and languages constructed, all to create an authentic experience of what it would be like to listen to the radio on the desert moon of Torobos.", img: "art/radio.png", link: "/radio"},
         { title: "Early Explorations (2022)", desc: "What does it mean to be human? What does it mean to be you?", img: "art/human.PNG", link: "#"},
         { title: "Cosmic Perspective (2022)", desc: "The night gave me back my wonder / my distance./ I have never felt so soft a glow / my body inextricably spread across / the field of the cosmos.", img: "art/starview.PNG", link: "#" },
         { title: "The Ocean Within (2024)", desc: "Knowing we can only see through our own frames of reference BUT being open to exploring others' oceans.", img: "art/underwater.PNG", link: "#"},
         { title: "Connection (2023)", desc: "Realization: the ultimate nature of human existence is connection", img: "art/connection.PNG", link: "#"},        
     ],
     space: [
-        { title: "Cosmic Source Separation", desc: "A highly flexible improvement on GMCA and other source separation techniques, using starlet transformations and GMMs.", img: "space/sources.png", link: "#"},
-        { title: "Microlensing ML Comparisons", desc: "Synthetic microlensing event generation pipeline with a high signal-to-noise ratio; then tested transformers, CNNs, MLPs, and simple least squares curve fitting for characterization.", img: "space/microlensing.png", link: "#" }
+        { title: "Cosmic Source Separation", desc: "A highly flexible improvement on GMCA and other source separation techniques, using starlet transformations and GMMs.", img: "space/sources.png", link: "https://sites.lsa.umich.edu/liacorrales/research/galactic-center/"},
+        { title: "Microlensing ML Comparisons", desc: "Synthetic microlensing event generation pipeline with a high signal-to-noise ratio; then tested transformers, CNNs, MLPs, and simple least squares curve fitting for characterization.", img: "space/microlensing.png", link: "https://github.com/star-skelly/microlensing" }
     ],
     writing: [
-        { title: "Awards Won", desc: "2026: Cooley Fiction Writing (1st), O4U People's Choice Award Lightning Talk (on interdisciplinary engineering) \n 2025: Cooley Fiction Writing (Honorable Mention), Roger M. Jones Poetry (3rd), Caldwell Original Performance (4th)\n 2024: Cooley Fiction Writing (2nd), Roger M Jones (Honorable Mention), Advancing Climate Education (3rd), Science As Art (Honorable Mention) \n2023: Caldwell Written Poetry (1st), Caldwell Original Performance (3rd), Hopwood Weisberg Poem (2nd)", img: "pics/ocean_stars.jpg", link: "#"},
+        { title: "Awards Won", desc: "2026: Cooley Fiction Writing (1st), O4U People's Choice Award Lightning Talk (on interdisciplinary engineering) \n 2025: Cooley Fiction Writing (Honorable Mention), Roger M. Jones Poetry (3rd), Caldwell Original Performance (4th)\n 2024: Cooley Fiction Writing (2nd), Roger M Jones (Honorable Mention), Advancing Climate Education (3rd), Science As Art (Honorable Mention) \n2023: Caldwell Written Poetry (1st), Caldwell Original Performance (3rd), Hopwood Weisberg Poem (2nd)", img: "pics/ocean_stars.jpg", link: "https://lsa.umich.edu/lswa/current-students/caldwell-poetry-award/caldwell-poetry-award-winners.html"},
         { title: "Current Projects", desc: "'Lost Kids' Last Stand: a novel about music, complicated love, and time. About 100 pages into the 3rd draft so far. \n Captured By Sunlight: a graphic novel about family, magic, and running from past mistakes. We're 5 chapters in so far (~ 150 pages). \n ... and many more smaller projects", img: "pics/multimedia.PNG", link: "#" }
     ]
 };
@@ -144,15 +145,21 @@ document.querySelectorAll('.interactive-logo').forEach(logo => {
             container.classList.remove('single-project-view');
         }
 
-        // 3. Render the project(s)
-        container.innerHTML = displayProjects.map(p => `
+        const isArt = activeData === 'art';
+        container.innerHTML = displayProjects.map(p => 
+            `
             <div class="project-card">
                 <div class="image-container">
                     <img src="${p.img}" alt="${p.title}">
                 </div>
                 <h3>${p.title}</h3>
                 <p>${p.desc}</p>
-                <a href="${p.link}" target="_blank">View Project</a>
+                <a href="${isArt && p.link === "#" ? "#" : p.link}" 
+                    class="${isArt && p.link === "#" ? "art-zoom-link" : ""}" 
+                    ${!isArt ? 'target="_blank"' : ''} 
+                    data-img="${p.img}">
+                    ${isArt && p.link === "#" ? '🔍 View Full Image' : 'View Project'}
+                </a>
             </div>
         `).join('');
 
@@ -166,6 +173,20 @@ document.querySelectorAll('.interactive-logo').forEach(logo => {
                 const imgSrc = c.querySelector('img').src;
                 const lb = document.getElementById('lightbox');
                 document.getElementById('lightbox-img').src = imgSrc;
+                lb.classList.remove('modal-hidden');
+            });
+        });
+
+        const artLinks = container.querySelectorAll('.art-zoom-link');
+        artLinks.forEach(link => {
+            link.addEventListener('click', (e) => {
+                e.preventDefault();
+                
+                const imgSrc = link.getAttribute('data-img');
+                const lb = document.getElementById('lightbox');
+                const lbImg = document.getElementById('lightbox-img');
+                
+                lbImg.src = imgSrc;
                 lb.classList.remove('modal-hidden');
             });
         });
